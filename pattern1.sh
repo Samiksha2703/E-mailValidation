7e0644504952c4804e26fcf952405b69fb533da2
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-04 16:43:45 +0530

    Added : E-mail regex with mandatory1

--- pattern1.sh
+++ pattern1.sh
@@ -1,3 +1,20 @@
 #!/bin/bash/
 
 echo "Welcome to regex"
+
+E_MAIL_ID="^[a-zA-Z0-9]+([.+-]*[a-zA-Z0-9]+)*$"
+
+echo "Enter the E-mail ID"
+
+read email_Id
+
+if [[ $email_Id =~ $E_MAIL_ID ]]
+then
+
+echo "Valid E-mail ID"
+
+else
+
+echo "Not a valid E-mail ID"
+
+fi
